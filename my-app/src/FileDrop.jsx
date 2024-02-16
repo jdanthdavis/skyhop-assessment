06a1c63453990511b0f2dcedffe78c5b449f4499
jdanthdavis <87504521+jdanthdavis@users.noreply.github.com>
2024-02-16 11:42:57 -0500

    Dropzone added and styled

--- my-app/src/FileDrop.jsx
+++ my-app/src/FileDrop.jsx
@@ -1,23 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, createRef } from 'react';
 import './FileDrop.css';
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Dropzone from 'react-dropzone';
+import { CiClock1 } from 'react-icons/ci';
 import Switch from '@mui/material/Switch';
 
-const FileDrop = () => {
+const FileDrop = ({ setUploadedFiles }) => {
   const [isChecked, setIsChecked] = useState(true);
+  const dropzoneRef = createRef();
+  const openBrowse = () => {
+    if (dropzoneRef.current) {
+      dropzoneRef.current.open();
+    }
+  };
   return (
     <div className="file-drop-container">
-      <h4>Select a manifest you'd like to import</h4>
-      {/* dropzone goes here */}
-
+      <div className="border" />
+      <div className="drop-zone-wrapper">
+        <div>
+          <strong>Select a manifest you'd like to import</strong>
+        </div>
+        <Dropzone
+          ref={dropzoneRef}
+          onDrop={(acceptedFiles) => {
+            setUploadedFiles(acceptedFiles);
+          }}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <section className="dropzone-section">
+              <div {...getRootProps({ className: 'dropzone' })}>
+                <input {...getInputProps()} />
+                <p>Drag & Drop Here Or Browse</p>
+              </div>
+              <button className="modal-btn" onClick={openBrowse}>
+                Upload Manifest
+              </button>
+            </section>
+          )}
+        </Dropzone>
+      </div>
       <div className="elapsed-data-container">
-        <span>Elapse Data Checking</span>
+        <div className="border" />
+        <span>
+          <strong>Elapse Data Checking</strong>
+        </span>
+
         <span className="dates">No Elapsed Dates!</span>
       </div>
-      <br />
       <div className="tolerance-window">
-        <span className="switch-label">Tolerance Window:</span>
+        <div className="border" />
+        <span>
+          <strong>Tolerance Window:</strong>
+        </span>
         <div className="switch-container">
           <FormGroup>
             <FormControlLabel
@@ -31,6 +66,7 @@ const FileDrop = () => {
             ></FormControlLabel>
           </FormGroup>
           <div className="tolerance">Select Tolerance Level</div>
+          <CiClock1 size={23} />
         </div>
       </div>
     </div>
--- my-app/src/UploadModal.jsx
+++ my-app/src/UploadModal.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import FileDrop from './FileDrop';
-import './UploadModal.css';
-
-const mockSelectOptions = ['mock', 'data'];
+import './UploadModal.scss';
+import ClientSelect from './ClientSelect';
 
 const UploadModal = () => {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
   return (
     <div className="modal">
       <div className="content">
@@ -12,16 +12,35 @@ const UploadModal = () => {
           <button className="modal-close-btn">X</button>
           <h1 className="modal-title">Document Upload</h1>
         </div>
+        <div className="main-container">
+          <div className="import-container">
+            <select>
+              <option hidden>Select Import Name:</option>
+              {uploadedFiles.map((x) => {
+                return <option value={x?.name}>{x?.name}</option>;
+              })}
+            </select>
+            <FileDrop
+              uploadedFiles={uploadedFiles}
+              setUploadedFiles={setUploadedFiles}
+            />
+          </div>
 
-        <div className="import-container">
-          <select>
-            <option hidden>Select Import Name:</option>
-            {mockSelectOptions.map((x) => {
-              return <option value={x}>{x}</option>;
-            })}
-          </select>
-          {/* <div className="border" /> */}
-          <FileDrop />
+          <ClientSelect />
+        </div>
+        <div className="modal-footer">
+          <div>
+            <strong>
+              Data in the import file is correct. Please press Continue to
+              import.
+            </strong>
+          </div>
+          <div className="modal-btn-wrapper">
+            <div className="modal-btn-container">
+              <button className="modal-btn">Continue Import</button>
+              <button className="modal-btn-cancel">Cancel</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
